d2a78f3d572f7b23703f84a42ed06fc267a464d1
Sanjay <kranthi0987@gmail.com>
2016-08-24 13:21:34 +0530

    student list api updated

--- app/src/main/java/app/managementapp/college/com/collegemanagement/api/CollegeManagementApiService.java
+++ app/src/main/java/app/managementapp/college/com/collegemanagement/api/CollegeManagementApiService.java
@@ -3,6 +3,7 @@ package app.managementapp.college.com.collegemanagement.api;
 
 import app.managementapp.college.com.collegemanagement.api.AcademicCalender.AcademicCalenderResponse;
 import app.managementapp.college.com.collegemanagement.api.AdmissionDetails.AdmissionDetailsResponse;
+import app.managementapp.college.com.collegemanagement.api.AttendanceDetails.AttendanceDetailsResponse;
 import app.managementapp.college.com.collegemanagement.api.FeedbackList.FeedbackListResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -34,5 +35,7 @@ public interface CollegeManagementApiService {
     @GET("ManagementService.svc/GetAdmissionDetails")
     Call<AdmissionDetailsResponse> getAdmissionDetails(@Header("Token") String token);
 
+    @GET("ManagementService.svc/GetAttendanceDetails")
+    Call<AttendanceDetailsResponse> getAttendanceDetails(@Header("Token") String token);
 
 }
