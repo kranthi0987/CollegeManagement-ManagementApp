f417bb9223d257b0bf68deaa50027aefc6affaa2
Sanjay <kranthi0987@gmail.com>
2016-08-24 19:59:20 +0530

    college management generator update 2

--- app/src/main/java/app/managementapp/college/com/collegemanagement/api/CollegeManagementApiService.java
+++ app/src/main/java/app/managementapp/college/com/collegemanagement/api/CollegeManagementApiService.java
@@ -6,7 +6,19 @@ import app.managementapp.college.com.collegemanagement.api.AdmissionDetails.Admi
 import app.managementapp.college.com.collegemanagement.api.AttendanceDetails.AttendanceDetailsResponse;
 import app.managementapp.college.com.collegemanagement.api.BranchOrCycle.BranchOrCycleResponse;
 import app.managementapp.college.com.collegemanagement.api.CollegeProfile.CollegeProfileResponse;
+import app.managementapp.college.com.collegemanagement.api.CourseFeedback.CourseFeedbackResponse;
+import app.managementapp.college.com.collegemanagement.api.CourseList.CourseListResponse;
+import app.managementapp.college.com.collegemanagement.api.ExamMark.ExamMarkResponse;
+import app.managementapp.college.com.collegemanagement.api.FeePaymentDetails.FeePaymentDetailsResponse;
 import app.managementapp.college.com.collegemanagement.api.FeedbackList.FeedbackListResponse;
+import app.managementapp.college.com.collegemanagement.api.Profiles.University.UniversityProfileResponse;
+import app.managementapp.college.com.collegemanagement.api.QualificationDetails.QualificationDetailsResponse;
+import app.managementapp.college.com.collegemanagement.api.Semester.SemesterResponse;
+import app.managementapp.college.com.collegemanagement.api.Staff.Department.DepartmentResponse;
+import app.managementapp.college.com.collegemanagement.api.Staff.StaffAttendance.StaffAttendanceResponse;
+import app.managementapp.college.com.collegemanagement.api.Staff.StaffFeedback.StaffFeedbackResponse;
+import app.managementapp.college.com.collegemanagement.api.Staff.StaffLeavesApplied.StaffLeavesAppliedResponse;
+import app.managementapp.college.com.collegemanagement.api.Staff.StaffList.StaffListResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -46,6 +58,39 @@ public interface CollegeManagementApiService {
     @GET("ManagementService.svc/GetCollegeProfile")
     Call<CollegeProfileResponse> getCollegeProfile(@Header("Token") String token, @Query("ID") Integer id);
 
-    @GET("ManagementService.svc/GetCousreFeedback")
+    @GET("ManagementService.svc/GetCourseFeedback")
+    Call<CourseFeedbackResponse> getCourseFeedback(@Header("Token") String token, @Query("StaffID") Integer staffid);
 
+    @GET("ManagementService.svc/GetCourseList")
+    Call<CourseListResponse> getCourseList(@Header("Token") String token);
+
+    @GET("ManagementService.svc/GetExamMark")
+    Call<ExamMarkResponse> getExamMarks(@Header("Token") String token, @Query("StudentID") Integer studentid);
+
+    @GET("ManagementService.svc/GetFeePaymentDetails")
+    Call<FeePaymentDetailsResponse> getFeePaymentDetails(@Header("Token") String token, @Query("StudentID") Integer studentid);
+
+    @GET("ManagementService.svc/GetUniversityProfile")
+    Call<UniversityProfileResponse> getUniversityProfile(@Header("Token") String token, @Query("ID") Integer id);
+
+    @GET("ManagementService.svc/GetQualificationDetails")
+    Call<QualificationDetailsResponse> getQualificationDetails(@Header("Token") String token, @Query("StudentID") Integer studentid);
+
+    @GET("ManagementService.svc/GetSemester")
+    Call<SemesterResponse> getSemester(@Header("Token") String token, @Query("CourseID") Integer courseid);
+
+    @GET("ManagementService.svc/GetDepartment")
+    Call<DepartmentResponse> getDepartment(@Header("Token") String token);
+
+    @GET("ManagementService.svc/GetStaffAttendance")
+    Call<StaffAttendanceResponse> getStaffAttendance(@Header("Token") String token, @Query("StaffID") String staffid);
+
+    @GET("ManagementService.svc/GetStaffFeedback")
+    Call<StaffFeedbackResponse> getStaffFeedback(@Header("Token") String token, @Query("Staffid") String staffid);
+
+    @GET("ManagementService.svc/GetStaffLeavesApplied")
+    Call<StaffLeavesAppliedResponse> getStaffLeavesApplied(@Header("Token") String token, @Query("Staffid") String staffid);
+
+    @GET("ManagementService.svc/GetStaffList")
+    Call<StaffListResponse> getStaffList(@Header("Token") String token, @Query("StaffName") String staffname, @Query("StaffCode") String staffcode);
 }
